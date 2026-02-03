6d73618ec6f9bfa6d22751c903e0da31637b18c8
Hamed Elghazzali <elyasisfire@hotmail.com>
2026-02-03 18:16:10 -0500

    added cash flow information as sql tables

---
+++ Cash_Flow_Data/Expenses.sql
@@ -0,0 +1,90 @@
+CREATE TABLE worker_pay(
+    worker_id INT,
+    worker_name VARCHAR(100),
+    hours_worked DECIMAL(2,1) NOT NULL DEFAULT 0.0,
+    pay DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE Electricity_Bill(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE food_cost(
+    billing_name VARCHAR(100),
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE cleaning_bill(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE miscellaneous_cost(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE maintenance_cost(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE water_cost(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE HVAC_cost(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE internet_cost(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE phone_bill(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE insurance_cost(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE taxes(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
+
+CREATE TABLE television_cost(
+    billing_name VARCHAR(100),
+    description VARCHAR(250) NOT NULL DEFAULT '',
+    cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    dates DATE
+)
