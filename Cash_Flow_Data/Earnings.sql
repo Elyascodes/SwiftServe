04e738c0d1b431ceb0a77f0f80d46fbdc681ea7c
Hamed Elghazzali <elyasisfire@hotmail.com>
2026-02-04 14:48:13 -0500

    added primary keys to all tables

--- Cash_Flow_Data/Earnings.sql
+++ Cash_Flow_Data/Earnings.sql
@@ -2,7 +2,8 @@ CREATE TABLE earnings_by_day(
     dates DATE,
     revenue DECIMAL(10,2) NOT NULL DEFAULT 0.00,
     cash_payments INT NOT NULL DEFAULT 0,
-    card_payments INT NOT NULL DEFAULT 0
+    card_payments INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (revenue)
 )
 
 CREATE TABLE earnings_by_week(
@@ -10,12 +11,14 @@ CREATE TABLE earnings_by_week(
     end_of_week DATE,
     revenue DECIMAL(10,2) NOT NULL DEFAULT 0.00,
     cash_payments INT NOT NULL DEFAULT 0,
-    card_payments INT NOT NULL DEFAULT 0
+    card_payments INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (revenue)
 )
 
 CREATE TABLE earnings_by_month(
     month DATE,
     revenue DECIMAL(10,2) NOT NULL DEFAULT 0.00,
     cash_payments INT NOT NULL DEFAULT 0,
-    card_payments INT NOT NULL DEFAULT 0
+    card_payments INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (revenue)
 )
--- Cash_Flow_Data/Expenses.sql
+++ Cash_Flow_Data/Expenses.sql
@@ -3,88 +3,101 @@ CREATE TABLE worker_pay(
     worker_name VARCHAR(100),
     hours_worked DECIMAL(2,1) NOT NULL DEFAULT 0.0,
     pay DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (worker_id)
 )
 
 CREATE TABLE Electricity_Bill(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE food_cost(
     billing_name VARCHAR(100),
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE cleaning_bill(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE miscellaneous_cost(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE maintenance_cost(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE water_cost(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE HVAC_cost(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE internet_cost(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE phone_bill(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE insurance_cost(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE taxes(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
 
 CREATE TABLE television_cost(
     billing_name VARCHAR(100),
     description VARCHAR(250) NOT NULL DEFAULT '',
     cost DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-    dates DATE
+    dates DATE,
+    PRIMARY KEY (billing_name)
 )
